86f849795fae76ae4fb78c5af9bc3d61d7f32650
Raj Kamdar <raj9235@gmail.com>
2017-03-02 01:42:22 +0530

    Added fetchlist

--- js/main.js
+++ js/main.js
@@ -7,16 +7,33 @@ function addToDo(e){
 		text:todoText
 	}
 
-	console.log(todo.text);
 
-	localStorage.setItem('test','Hey');
-	alert(localStorage.getItem('test'));
-	localStorage.removeItem('test');
-	alert(localStorage.getItem('test'));
+	if(localStorage.getItem('todolist')===null){
+		var todolist=[];
+		todolist.push(todo);
 
+		localStorage.setItem("todolist",JSON.stringify(todolist));
+	}
+	else{
+		var todolist=JSON.parse(localStorage.getItem('todolist'));
+		todolist.push(todo);
+		localStorage.setItem("todolist",JSON.stringify(todolist));
+	}
+
+
+	//prevents auto-refresh
 	e.preventDefault();
 }
-
+function fetchList(){
+	var todolist=JSON.parse(localStorage.getItem('todolist'));
+	var listdiv=get('todolist');
+	listdiv.innerHTML='';
+	for(var i=0;i<todolist.length;i++){
+		var text=todolist[i].text;
+		console.log(text);
+		listdiv.innerHTML+='<div class="well">'+'<h4>'+text+'</h4></div>'
+	}
+}
 function get(x){
 	return document.getElementById(x);
 }
